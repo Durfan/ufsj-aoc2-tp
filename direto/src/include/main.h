4b7db7923228daa36cc7256f46f687df70afd744
Durfan <31577607+Durfan@users.noreply.github.com>
2019-06-06 07:38:55 -0300

    toc

--- direto/src/include/main.h
+++ direto/src/include/main.h
@@ -14,9 +14,9 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
-#include <sys/time.h>
 #include <assert.h>
 
+#include <sys/time.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <unistd.h>
